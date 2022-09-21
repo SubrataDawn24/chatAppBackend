84618f8077fb66d0d64b16f79cddbe3f1bfd1d6e
Subrata Dawn <72289319+SubrataDawn24@users.noreply.github.com>
2022-09-21 23:30:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // Node server which will handle socket io connections
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+
+const server = http.createServer(app);
+const  {Server}  = require("socket.io");
+
+const io = new Server(server);
+
+
+var port = process.env.PORT || 8000;
+
+server.listen(port,()=>{
+    console.log("server is listening",port)
+});
 const port = process.env.PORT || 8800;
-const io = require('socket.io')(port,{
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }});
+// const io = require('socket.io')(port,{
+//     cors: {
+//       origin: "*",
+//       methods: ["GET", "POST"],
+//       allowedHeaders: ["my-custom-header"],
+//       credentials: true
+//     }});
 
 const user = {};
 
