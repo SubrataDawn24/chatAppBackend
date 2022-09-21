02b6d392e5296f566324049f48bac3476891c3c6
Subrata <adithadawn@gmail.com>
2022-09-21 18:41:19 +0530

    chat app

---
+++ index.js
@@ -0,0 +1,28 @@
+// Node server which will handle socket io connections
+
+const io = require('socket.io')(8800,{
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }});
+
+const user = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name=>{
+        console.log("welcome ", name)
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    });
+    socket.on('send', message =>{
+        socket.broadcast.emit('recieve', {message: message, name: user[socket.id]})
+    });
+    
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', user[socket.id]);
+        delete user[socket.id]
+    });
+
+})
